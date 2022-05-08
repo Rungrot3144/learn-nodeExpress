3ee06e7a5f073928add30df9e55b1cd6b88ba340
Rungrot3144 <105099026+Rungrot3144@users.noreply.github.com>
2022-05-08 12:58:49 +0700

    indexEjs

--- app.js
+++ app.js
@@ -10,8 +10,15 @@ const PORT = process.env.PORT||4000;
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views");
+app.set("view engine","ejs");
+
 app.get("/",(req,res)=>{
-    res.send('Hello');
+    // res.send('Hello');
+    // res.render ( "index" );
+    // res.render ( "homepage" );
+    // res.render('index',{username:'Artery111'});
+    res.render('index',{username:'Artery111',customers:['kitti','kittikorn','kitty']});
 })
 
 app.listen(PORT,()=>{
